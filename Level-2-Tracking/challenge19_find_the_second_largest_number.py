956285c7b8d6dbc603065c20abdb7855cbf31ea7
Suraj Chaudhary <chaudharysuraz999@gmail.com>
2026-06-08 18:48:37 +0545

    feat: solve second number challenge

--- Level-2-Tracking/challenge19_find_the_second_largest_number.py
+++ Level-2-Tracking/challenge19_find_the_second_largest_number.py
@@ -1,14 +1,15 @@
-arr = [20, 4, 6, 8, 1]
+arr = [20, 30, 10, 25]
 
 largest_number = None
 second_largest_number = None
 
 for num in arr:
     if largest_number is None or num > largest_number:
+        second_largest_number = largest_number
         largest_number = num
     elif largest_number > num:
         if second_largest_number is None or second_largest_number < num:
-            second_largest_number = num
+            second_largest_number = num 
 
 print(second_largest_number)
 print(largest_number)
