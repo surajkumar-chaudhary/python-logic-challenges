d04bfd5eca56d038f7765381e5b7c3d7bed4e6db
Suraj Chaudhary <chaudharysuraz999@gmail.com>
2026-06-26 14:41:31 +0545

    Add challenge54 Find shortest word

---
+++ Level-4-String/challenge54_find_shortest_word.py
@@ -0,0 +1,15 @@
+text = "love in Python programming in"
+
+words = text.split()
+multiple_shortest_word = []
+length = None
+
+for word in words:
+    if length is None or len(word) < length:
+        length  = len(word)
+        multiple_shortest_word = [word]
+    elif length == len(word):
+        multiple_shortest_word.append(word)
+
+print("Shortest words:", multiple_shortest_word)
+print("Lnegth of the word:", length)
